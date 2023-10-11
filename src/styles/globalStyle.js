33172c33fea7d309ec0f82b7e715fe6f1e56c8d4
lsj0202 <w1004123@gmail.com>
2023-10-11 15:46:42 +0900

    build project

---
+++ src/styles/globalStyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  text-decoration: none;
+}
+
+input {
+  border: none;
+	outline: none;
+}
+`;
+
+export default GlobalStyle;
