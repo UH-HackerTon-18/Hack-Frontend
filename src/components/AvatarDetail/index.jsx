352387ec182016a667b9657c44f012d9d8b8729a
이아론 <aron0613@naver.com>
2023-10-11 23:03:50 +0900

    Add : Add detail components

---
+++ src/components/AvatarDetail/index.jsx
@@ -0,0 +1,74 @@
+import TitleBox from 'components/common/TitleBox'
+import * as S from './style'
+import Button from 'components/common/Button'
+import { AvatarCategoryData } from 'assets/data/AvatarCategoryData'
+
+function Category({ title, data }) {
+    return (
+        <S.CategoryWrapper>
+            <S.CategoryTitle>{title}</S.CategoryTitle>
+            <S.CategoryData>{data}</S.CategoryData>
+        </S.CategoryWrapper>
+    )
+}
+
+export default function AvatarDetail({ }) {
+
+    return (
+        <S.DetailTemplate>
+            <S.AvatarDetailWrapper>
+                <TitleBox
+                    title="아바타"
+                    subTitle="간단하게 아바타를 만들어보세요"
+                />
+                <S.imgboxWrapper>
+                    <S.imgWrapper>
+                        <img src="aa" alt='' />
+                    </S.imgWrapper>
+                </S.imgboxWrapper>
+                <S.subTitle>아바타 소개</S.subTitle>
+                <S.CategoryListWrapper>
+                    {AvatarCategoryData.map((item) => (
+                        <Category
+                            title={item.name}
+                            data="미정"
+                        />
+                    ))}
+                </S.CategoryListWrapper>
+                <S.ButtonListWrapper>
+                    <Button
+                        backgroundColor="#272727"
+                        color="#FFF"
+                        fontWeight="800"
+                        width="230"
+                        height="40"
+                        fontSize="18px"
+                        borderRadius="5"
+                        border="none"
+                        onClick={() => { }}
+                    >채팅하기</Button>
+                    <Button
+                        backgroundColor="#FFF"
+                        color="#272727"
+                        fontWeight="800"
+                        width="170"
+                        height="40"
+                        fontSize="18px"
+                        borderRadius="5"
+                        onClick={() => { }}
+
+                    >오픈API로 사용하기</Button> <Button
+                        backgroundColor="#FFF"
+                        color="#272727"
+                        fontWeight="800"
+                        width="100"
+                        height="40"
+                        fontSize="18px"
+                        borderRadius="5"
+                        onClick={() => { }}
+                    >돌아가기</Button>
+                </S.ButtonListWrapper>
+            </S.AvatarDetailWrapper>
+        </S.DetailTemplate>
+    )
+}
--- src/styles/color.js
+++ src/styles/color.js
@@ -1,6 +1,15 @@
 const gray = {
   100: '#F7F8FA',
   200: '#DADADA',
+  300: '#676767',
+  400: `#656B80`,
 };
+const black = {
+  100: '#000000',
+  200: '#272727',
+}
+const white = {
+  100: '#fff',
+}
 
-export default gray;
+export default { gray, black, white };
