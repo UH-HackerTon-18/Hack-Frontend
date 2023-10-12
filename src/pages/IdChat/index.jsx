dc52ff6d8cbb5b6b2947e759ccf115437c3132c7
이아론 <aron0613@naver.com>
2023-10-12 23:37:59 +0900

    Change text

--- src/pages/IdChat/index.jsx
+++ src/pages/IdChat/index.jsx
@@ -1,11 +1,15 @@
 import Container from "components/common/Container"
 import * as S from "./style"
+import TitleBox from "components/common/TitleBox"
 export default function IdChat() {
     return (
-        <Container>
-            <S.IdChatWrapper>
-                fghy54e
-            </S.IdChatWrapper>
-        </Container>
+        <>
+            <Container>
+                <S.IdChatWrapper>
+                    <TitleBox title="id로 채팅하기" subTitleVisible={false} />
+
+                </S.IdChatWrapper>
+            </Container>
+        </>
     )
 }
